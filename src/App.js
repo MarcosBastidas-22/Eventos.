d9e5de42d59ce8215c459f9e8ec149b444400a43
marcos <marcosbastidas100@gmail.com>
2019-09-01 07:24:01 -0430

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import  CategoriasProvider from './context/CategoriasContext';
+import Formulario from './components/Formulario';
+import EventosProvider from './context/contextEventos';
+import ListaEventos from './components/ListaEventos';
+
+function App() {
+  return (
+    <EventosProvider>
+      <CategoriasProvider>
+        <Header />
+
+        <div className="uk-container">
+          <Formulario />
+
+          <ListaEventos />
+        </div>
+
+      </CategoriasProvider>
+    </EventosProvider>
+  );
+}
+
+export default App;
